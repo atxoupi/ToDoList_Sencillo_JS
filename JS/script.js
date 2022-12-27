6c535d2833fb2d12c632c24b32109851bc5ad7f7
Andres <andres.hermelo@gmail.com>
2022-12-27 12:49:50 +0100

    Falta solucionar Borrador Item y estilos finales

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,37 @@
 const enviar = document.getElementById("enviar");
+const reset = document.getElementById("reset");
 const tarea=document.getElementById("tarea");
 const lista=document.getElementById("lista");
+const borrar=(cosa)=>{
+    listado=listado.filter(item=>item===cosa);
+    mostrar_tabla(listado);
+};
 let listado=[];
 
 enviar.addEventListener ('click', (evento) => {
-    evento.preventDefault();
     let nuevo=tarea.value;
-    listado.push(nuevo);
-    tarea.value="";
-    tarea.focus();
-    console.log(listado);
+    if(nuevo===""){
+        alert("debes introducir un valor");
+    }
+    else{
+        listado.push(nuevo);
+        tarea.value="";
+        tarea.focus();
+        console.log(listado);
+        mostrar_tabla(listado);
+    }
+});
+reset.addEventListener('click',(e)=>{
+    listado=[];
     mostrar_tabla(listado);
 });
+
 
 function mostrar_tabla(array){
     let contenido="";
     for(let i=0; i<array.length; i++){
-        contenido+="<li>"+array[i]+"</li>";
+        // contenido+="<li>"+array[i]+"<button id='del' onclick={() =>deleteItem("+i+")}>Eliminar</button></li>";
+        contenido+=`<li>${array[i]}<button id="del" onclick="${borrar(array[i])}">DEL</button></button> </li>`;
     }
     lista.innerHTML=contenido;
 };
