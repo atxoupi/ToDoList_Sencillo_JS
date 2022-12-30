2c4b3fb58a9be5deac78e74ca5805ba8adaf3314
Andres <andres.hermelo@gmail.com>
2022-12-30 11:18:50 +0100

    Intentando implementar el borrado

--- JS/script.js
+++ JS/script.js
@@ -2,10 +2,9 @@ const enviar = document.getElementById("enviar");
 const reset = document.getElementById("reset");
 const tarea=document.getElementById("tarea");
 const lista=document.getElementById("lista");
-const borrar=(cosa)=>{
-    listado=listado.filter(item=>item===cosa);
-    mostrar_tabla(listado);
-};
+const removeElement=document.getElementsByTagName("span");
+
+
 let listado=[];
 
 enviar.addEventListener ('click', (evento) => {
@@ -19,6 +18,12 @@ enviar.addEventListener ('click', (evento) => {
         tarea.focus();
         console.log(listado);
         mostrar_tabla(listado);
+        console.log(removeElement);
+        for(tipo in removeElement){
+            console.log("que devuelve el span...");
+            console.log(tipo);
+        }
+        console.log(reset);
     }
 });
 reset.addEventListener('click',(e)=>{
@@ -26,12 +31,26 @@ reset.addEventListener('click',(e)=>{
     mostrar_tabla(listado);
 });
 
+// function borrar(cosa){
+//     console.log(listado);
+//     listado=listado.filter((item)=>item!==cosa);
+//     // mostrar_tabla(listado);
+// };
+
+// removeElement.addEventListener('click',(e)=>{
+//     delItem=e.target;
+//     delItem.parentNode.parentNode.removeChild(delItem.parentNode);
+//     listado=listado.filter((item)=>item!==delItem.parentNode.value);
+//     console.log("Eliminando...");
+// });
 
 function mostrar_tabla(array){
     let contenido="";
     for(let i=0; i<array.length; i++){
-        // contenido+="<li>"+array[i]+"<button id='del' onclick={() =>deleteItem("+i+")}>Eliminar</button></li>";
-        contenido+=`<li>${array[i]}<button id="del" onclick="${borrar(array[i])}">DEL</button></button> </li>`;
+        // contenido+="<li>"+array[i]+"<span id='del' >Eliminar</span></li>";
+        contenido+=`<li>${array[i]}<button id="del" >DEL</button> </li>`;
+        // onClick=${() => borrar(array[i])}
     }
     lista.innerHTML=contenido;
 };
+
