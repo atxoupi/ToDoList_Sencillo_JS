d02f562b182170c79dd0a466801d4cb5b45cd5aa
Andres <andres.hermelo@gmail.com>
2022-12-30 12:16:28 +0100

    Estilos Mejorados, añadida funcionalidad check

--- JS/script.js
+++ JS/script.js
@@ -2,10 +2,17 @@ const enviar = document.getElementById("enviar");
 const reset = document.getElementById("reset");
 const tarea=document.getElementById("tarea");
 const lista=document.getElementById("lista");
-const removeElement=document.getElementsByTagName("span");
+const taskList=document.getElementById("taskList");
 
+let data = localStorage.getItem("TODO");
+let listado;
+if(data){
+    listado = JSON.parse(data);
+    mostrar_tabla(listado); 
+}else{
+    listado = [];
+}
 
-let listado=[];
 
 enviar.addEventListener ('click', (evento) => {
     let nuevo=tarea.value;
@@ -17,33 +24,29 @@ enviar.addEventListener ('click', (evento) => {
         tarea.value="";
         tarea.focus();
         console.log(listado);
+        localStorage.setItem("TODO", JSON.stringify(listado));
         mostrar_tabla(listado);
     }
 });
 reset.addEventListener('click',(e)=>{
     listado=[];
+    localStorage.clear();
+    location.reload();
     mostrar_tabla(listado);
 });
-
-// function borrar(cosa){
-//     console.log(listado);
-//     listado=listado.filter((item)=>item!==cosa);
-//     // mostrar_tabla(listado);
-// };
-
-// removeElement.addEventListener('click',(e)=>{
-//     delItem=e.target;
-//     delItem.parentNode.parentNode.removeChild(delItem.parentNode);
-//     listado=listado.filter((item)=>item!==delItem.parentNode.value);
-//     console.log("Eliminando...");
-// });
 
 function mostrar_tabla(array){
     let contenido;
     let res=array.map((task)=>{
-        return `<li>${task}</li>`;
+        return `<li class="item"> <i class="fa-regular fa-circle"></i> ${task}</li>`;
     });
     contenido=res.join(" ");
-    lista.innerHTML=contenido;
+    taskList.innerHTML=contenido;
+    console.log(taskList);
 };
 
+taskList.addEventListener("click", function(event){
+    const element = event.target.firstElementChild; 
+    element.className==="fa-regular fa-circle"?element.className="fa-regular fa-circle-xmark":element.className="fa-regular fa-circle";
+    console.log(element);
+});
