59902046bde23ee43b005763cd979609733587a1
Andres <andres.hermelo@gmail.com>
2022-12-30 13:17:38 +0100

    Implementada funcion de borrado

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,7 @@ const reset = document.getElementById("reset");
 const tarea=document.getElementById("tarea");
 const lista=document.getElementById("lista");
 const taskList=document.getElementById("taskList");
+const removeElement=document.getElementById("papelera");
 
 let data = localStorage.getItem("TODO");
 let listado;
@@ -38,16 +39,26 @@ reset.addEventListener('click',(e)=>{
 function mostrar_tabla(array){
     let contenido;
     let res=array.map((task)=>{
-        return `<li class="item"> <i class="fa-regular fa-circle"></i> ${task}</li>`;
+        return `<li class="item"> <i class="fa-regular fa-circle"></i> ${task}<span class="trash"><i id="papelera" class="fa-solid fa-trash"></i></span> </li>`;
     });
     contenido=res.join(" ");
     taskList.innerHTML=contenido;
-    console.log(taskList);
 };
 
 taskList.addEventListener("click", function(event){
-    const element = event.target.firstElementChild; 
-    element.className==="fa-regular fa-circle"?element.className="fa-regular fa-circle-xmark":element.className="fa-regular fa-circle";
-    element.parentNode.className === "item"? element.parentNode.className += " checked": element.parentNode.className = "item";
-    console.log(element.parentNode);
+    const probando= event.target;
+    if(probando.getAttribute("id")==="papelera"){
+        let deleteItem=probando.parentNode;
+        taskList.removeChild(deleteItem.parentNode);
+        let eliminado=probando.parentNode.parentNode.textContent;
+        listado=listado.filter((element)=> element!==eliminado.trim());
+        localStorage.clear();
+        localStorage.setItem("TODO", JSON.stringify(listado));
+    }else{
+        const element = event.target.firstElementChild; 
+        element.className==="fa-regular fa-circle"?element.className="fa-regular fa-circle-xmark":element.className="fa-regular fa-circle";
+        element.parentNode.className === "item"? element.parentNode.className += " checked": element.parentNode.className = "item";
+    }
+    
 });
+
